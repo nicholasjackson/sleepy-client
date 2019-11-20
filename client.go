9ee81efcce9f4bf70234b311e1e3e681845259bd
Nic Jackson <jackson.nic@gmail.com>
2019-11-20 05:48:38 -0800

    Add debug / prod mode using build flags because that is totally obvious

--- client.go
+++ client.go
@@ -10,14 +10,37 @@ type HTTP struct{}
 
 // POST some stuff to a URL
 func (h *HTTP) POST(uri string, data []byte) (*http.Response, error) {
+	r, err := http.NewRequest(http.MethodPost, uri, data)
+	if err != nil {
+		return nil, err
+	}
 
-	time.Sleep(30 * time.Second)
-	return &http.Response{}, nil
+	return h.Do(r)
 }
 
 // GET some stuff to a URL
 func (h *HTTP) GET(uri string, data []byte) (*http.Response, error) {
+	r, err := http.NewRequest(http.MethodPost, uri, data)
+	if err != nil {
+		return nil, err
+	}
 
-	time.Sleep(30 * time.Second)
+	return h.Do(r)
+}
+
+func (h *HTTP) Do(r *http.Request) (*http.Response, error) {
+	// If we are running localy then mock this reponse for easy testing
+	// behaviour is switched based on build flag go build -tags prod
+
+	// Against popular opinion, I decided to bury the behaviour of this module
+	// to make testing super easy.
+	// Nic - Genius lead developer
+
+	// TODO implement the actual work
+	// This is fine for now nobody should be using this library in prod
+	if isProd {
+		time.Sleep(30 * time.Second)
+	}
+
 	return &http.Response{}, nil
 }
---
+++ vars_prod.go
@@ -0,0 +1,5 @@
+// +build prod
+
+package client
+
+var isProd = true
